e3ae94e7702dae9c9f1d0947d9f272ae5a6dafbd
AmosMwangi <amosmwangi101@gmail.com>
2019-10-28 04:25:06 +0300

    made some material imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MaDialog } from '@angular/material';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @Component({
