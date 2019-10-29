ca5cc6e66bb67889e6b0bdb0d8cef89788101bf3
AmosMwangi <amosmwangi101@gmail.com>
2019-10-29 08:45:54 +0300

    made date count and MIT licence

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1,'Amos Mwangi','Abraham Lincon', 'Find an online version and watch merlin find his son',new Date(2019,9,14),0,0),
-    new Quote(2,'Kayte spark','Abraham Lincon','I have to buy cookies for the parrot',new Date(2019,6,9),0,0),
-    new Quote(3,'Migos Qwevo','Abraham Lincon','Diana has her birthday coming up soon',new Date(2019,1,12),0,0),
+    new Quote(1,'Amos Mwangi','Abraham Lincon', 'When you cant find sunshine, be the sunshine',new Date(2019,9,14),0,0),
+    new Quote(2,'Daisy spark','Isaac Blue','If your goals dont scare you. They arent big enough.',new Date(2019,6,9),0,0),
+    new Quote(3,'Victor Teka','Amos Mwangi','Dont wait for opportunity. Creat it.',new Date(2019,1,12),0,0),
   ];
 
   deleteQuote(isComplete, index){
@@ -24,7 +24,7 @@ export class QuoteComponent implements OnInit {
     }
   }
 
-  //find most voted quote
+
   bestQuote() {
     let upVoteArr: number[] = [];
     for (let i of this.quotes) {
